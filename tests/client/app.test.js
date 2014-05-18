b9a6dded99202f01752975ce6eb468a0e55a5833
Cory <cory.armbrecht@gmail.com>
2014-05-18 11:36:10 -0400

    syncing, Adding untracked files

---
+++ tests/client/app.test.js
@@ -0,0 +1,14 @@
+describe('App', function(){
+	it('bootstraps', function(done){
+
+	});
+	it('loads most recent location data', function(){
+
+	});
+	it('loads news', function(){
+
+	});
+	it('loads notification data', function(){
+
+	});
+});
---
+++ tests/client/share.test.js
@@ -0,0 +1,8 @@
+describe('Share Widget', function(){
+	it('shows on show:share event', function(){
+
+	});
+	it('can connect to fb', function(){
+
+	});
+});
